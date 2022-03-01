d533e6418979797267c09551d571e05e3d87bdaa
Christophe Debruyne <christophe.debruyne@gmail.com>
2022-03-01 22:36:49 +0100

    Updates for the course

--- www/exemple-form.php
+++ www/exemple-form.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -6,29 +7,29 @@
 
 <body>
     <form method="post" action="exemple-form.php">
-        <input type="text" name="nom" placeholder="Nom" required />
-        <input type="text" name="prenom" placeholder="Prenom" required />
-        <input type="submit" value="Soumettre" />
+        <input type="text" name="nom" placeholder="Nom" required>
+        <input type="text" name="prenom" placeholder="Prénom" required>
+        <input type="submit" value="Soumettre">
     </form>
     <p>
         <?php
         if (isset($_POST['nom'])) {
-            echo "Vous avez ecrit " . $_POST['nom'] . " " . $_POST['prenom'];
+            echo "Vous avez écrit " . $_POST['nom'] . " " . $_POST['prenom'];
         }
         ?>
     </p>
 
     <form method="post" action="exemple-form.php">
         <select name="liste">
-            <option value="element1"> nom_element1 </option>
-            <option value="element2"> nom_element2 </option>
-            <option value="element3"> nom_element3 </option>
+            <option value="element1">nom_element1</option>
+            <option value="element2">nom_element2</option>
+            <option value="element3">nom_element3</option>
         </select>
-        <input type="submit" value="Soumettre" />
+        <input type="submit" value="Soumettre">
     </form>
     <?php
     if (isset($_POST['liste'])) {
-        echo "Vous avez ecrit nom_" . $_POST['liste'];
+        echo "<p>Vous avez écrit nom_" . $_POST['liste'] . "</p>";
     }
     ?>
 
--- www/exemple-php-mysql.php
+++ www/exemple-php-mysql.php
@@ -1,23 +1,21 @@
+<!DOCTYPE html>
 <html>
-<!-- connexion a la base de donnees -->
-<?php
-$bdd = new PDO('mysql:host=ms8db;dbname=groupXX;charset=utf8', 'groupXX', 'secret');
-?>
-
-<head>
-    <title>Départements</title>
-</head>
-
-<body>
-    <h1>Départements</h1>
+    <!-- Connexion a la base de données -->
     <?php
-    /*$req contient les tuples de la requete*/
-    $req = $bdd->query('SELECT * FROM department');
-    /*On affiche tous les resultats de la requete*/
-    while ($tuple = $req->fetch()) {
-        echo "<p>" . $tuple['DNO'] . " " . $tuple['DNAME'] . "</p>";
-    }
+    $bdd = new PDO('mysql:host=db;dbname=groupXX;charset=utf8', 'groupXX', 'secret');
     ?>
-</body>
-
+    <head>
+        <title>Départements</title>
+    </head>
+    <body>
+        <h1>Départements</h1>
+        <?php
+        /*$req contient les tuples de la requête*/
+        $req = $bdd->query('SELECT * FROM department');
+        /*On affiche tous les résultats de la requête*/
+        while ($tuple = $req->fetch()) {
+            echo "<p>" . $tuple['DNO'] . " " . $tuple['DNAME'] . "</p>";
+        }
+        ?>
+    </body>
 </html>
